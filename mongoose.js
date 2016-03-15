5fbb041be4600ca7ad0bcd8c0b560eb2c3f76a94
Mark Neumann <markneumann@gmail.com>
2016-03-15 15:44:06 -0700

    completed exercise

--- mongoose.js
+++ mongoose.js
@@ -24,7 +24,7 @@ mongoose.connect('mongodb://localhost/Dashboard');
 var AnimalSchema = new mongoose.Schema({
     name: String,
     location: String,
-    age: Number, },
+    age: Number, } ,
     { timestamps: {
         createdAt: 'created',
         updatedAt: 'LMD'
@@ -43,15 +43,22 @@ app.get('/', function(req, res) {                      //show them all
             console.log(err);
             //res.render("mongooses", {err: err});
         } else {
-            console.log(animals);
+            //console.log(animals);
             res.render("mongooses", {animals: animals});
         }
     });
 });
 
+// new request
+app.get('/mongooses/new', function(req, res) {                    //show blank edit form
+    console.log("new path");
+    res.render("new", {title: "New one"});
+});
+
 // id request
-app.get('/mongooses/:id', function(req, res) {                    //show just one
-    Animal.find({id: req}, function(err, animals){
+app.get('/mongooses/:id', function(req, res) {
+    console.log("id path");                                     //show just one
+    Animal.find({_id: req.params.id }, function(err, animals){
         if(err){
             console.log(err);
             res.render("mongooses", {err: err});
@@ -62,15 +69,9 @@ app.get('/mongooses/:id', function(req, res) {                    //show just on
     });
 });
 
-// new request
-app.get('/mongooses/new', function(req, res) {                    //show blank edit form
-    console.log("new path");
-    //res.render("new", {title: "New one"});
-});
-
 // Post new Animal
 app.post('/mongooses', function(req, res) {             // save new animal
-    console.log("POST DATA", req.body);
+    console.log("POST NEW", req.body);
     // This is where we would add the user from req.body to the database.
     var animal = new Animal({
         name: req.body.name,
@@ -107,38 +108,35 @@ app.get('/mongooses/:id/edit', function(req, res) {                //show popula
 
 // Post Edits to existing animal Animal
 app.post('/mongooses/:id', function(req, res) {         // save edit form
-    console.log("POST DATA", req.body);
+    console.log("Update DATA", req.body);
     // This is where we would add the user from req.body to the database.
-    var animal = new Animal({
-        name: req.body.name,
-        location: req.body.location,
-        age: req.body.age
-    });
-    console.log(animal);
+    console.log(req.params);
     // Try to save that new user to the database (
     //this is the method that actually inserts into the db) and
     // run a callback function with an error (if any) from the operation.
-    animal.save(function(err) {
+    Animal.findByIdAndUpdate(req.params.id, {$set: req.body } , function(err, animal) {
         // if there is an error console.log that something went wrong!
         if (err) {
             console.log('something went wrong');
             res.render("edit", {err: err});
         } else { // else console.log that we did well and then redirect root route
-            console.log('successfully added a animal!');
-            res.redirect('/');
+            console.log('successfully updated a animal!');
+            console.log("/mongooses/" + req.params.id);
+            res.redirect('/mongooses/'+req.params.id);
         }
     });
 });
 
 // destroy animal (record, not a real animal ;-)
-app.get('/mongooses/:id/destroy', function(req, res) {                //destroy one
-    Animal.remove({id: req}, function(err, animals){
+app.get('/mongooses/:id/destroy', function(req, res) {
+    console.log("destroy path");               //destroy one
+    Animal.remove({_id: req.params.id }, function(err, animals){
         if(err){
             console.log(err);
             res.render("mongooses", {err: err});
         } else {
             console.log(animals);
-            res.render("mongooses", {animals: animals});
+            res.redirect('/');
         }
     });
 });
